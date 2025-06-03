860930bd5be9e6d41e1cf50d94991cfb72f6aab4
=@ssreynoso <s.reynosodunjo@gmail.com>
2025-06-03 13:40:59 -0300

    feat: zenmode

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        require("zen-mode").setup({
+            window = {
+                width = 0.85, -- 85% del ancho de la pantalla
+                options = {
+                    number = true,
+                    relativenumber = false,
+                    wrap = false,
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").toggle()
+        end)
+    end,
+}
