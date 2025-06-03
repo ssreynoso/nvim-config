cd6dbf4673cc80d2bfbd1b1d8c6a169cac507d5f
=@ssreynoso <s.reynosodunjo@gmail.com>
2025-06-03 12:14:37 -0300

    Aumenta timeout de formateo

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -24,7 +24,7 @@ return {
             format_on_save = {
                 lsp_fallback = true,
                 async = false,
-                timeout_ms = 500,
+                timeout_ms = 2000,
             },
         })
 
@@ -32,7 +32,7 @@ return {
             conform.format({
                 lsp_fallback = true,
                 async = false,
-                timeout_ms = 500,
+                timeout_ms = 2000,
             })
         end, { desc = "Format file or range (in visual mode)" })
     end,
