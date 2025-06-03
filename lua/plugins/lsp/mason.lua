0a5b52c262b48fffed6b5f8ca91b5d53555cb0b2
=@ssreynoso <s.reynosodunjo@gmail.com>
2025-06-03 13:09:57 -0300

    rollback de versiones mason + setup_handlers

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,53 +1,113 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+    {
+        "williamboman/mason.nvim",
+        version = "1.11.0",
+        dependencies = {
+            {
+                "williamboman/mason-lspconfig.nvim",
+                version = "1.32.0",
+            },
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        config = function()
+            -- Importar plugins
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+            local mason_tool_installer = require("mason-tool-installer")
+            local lspconfig = require("lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+            -- Configurar Mason
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
 
-		local mason_tool_installer = require("mason-tool-installer")
+            -- Configurar Mason LSPConfig
+            mason_lspconfig.setup({
+                automatic_installation = false,
+                ensure_installed = {
+                    "ts_ls",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "jsonls",
+                    "lua_ls",
+                    "graphql",
+                    "emmet_ls",
+                    "prismals",
+                    "rust_analyzer",
+                    "pyright",
+                },
+            })
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+            -- Configurar Mason Tool Installer
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    "prettier",
+                    "stylua",
+                    "eslint_d",
+                },
+            })
 
-		mason_lspconfig.setup({
-			automatic_installation = true,
-			-- list of servers for mason to install
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"jsonls",
-				-- "lua_ls",
-				-- "graphql",
-				-- "emmet_ls",
-				"prismals",
-				"rust_analyzer",
-				"pyright",
-			},
-		})
+            -- Configurar capacidades de autocompletado
+            local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"eslint_d",
-			},
-		})
-	end,
+            -- Configurar handlers para servidores LSP
+            mason_lspconfig.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+                ["jsonls"] = function()
+                    lspconfig.jsonls.setup({
+                        settings = {
+                            json = {
+                                format = { enable = false },
+                                validate = { enable = true },
+                            },
+                        },
+                    })
+                end,
+                ["lua_ls"] = function()
+                    lspconfig.lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = { globals = { "vim" } },
+                                completion = { callSnippet = "Replace" },
+                            },
+                        },
+                    })
+                end,
+                ["graphql"] = function()
+                    lspconfig.graphql.setup({
+                        capabilities = capabilities,
+                        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+                    })
+                end,
+                ["emmet_ls"] = function()
+                    lspconfig.emmet_ls.setup({
+                        capabilities = capabilities,
+                        filetypes = {
+                            "html",
+                            "typescriptreact",
+                            "javascriptreact",
+                            "css",
+                            "sass",
+                            "scss",
+                            "less",
+                            "svelte",
+                        },
+                    })
+                end,
+            })
+        end,
+    },
 }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,34 +1,35 @@
-
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- íconos
-	},
-	init = function()
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
-		require("keymaps.nvim-tree").setup()
-	end,
-	config = function()
-		require("nvim-web-devicons").setup()
-
-		require("nvim-tree").setup({
-			view = {
-				side  = "right",
-				width = 40,
-				preserve_window_proportions = true,
-			},
-			renderer = {
-				icons = {
-					show = {
-						file         = true,
-						folder       = true,
-						folder_arrow = true,
-						git          = false,
-					},
-				},
-			},
-		})
-	end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons", -- íconos
+    },
+    init = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        require("keymaps.nvim-tree").setup()
+    end,
+    config = function()
+        require("nvim-tree").setup({
+            view = {
+                side = "right",
+                width = 40,
+                preserve_window_proportions = true,
+            },
+            renderer = {
+                icons = {
+                    show = {
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                        git = false,
+                    },
+                },
+            },
+            filters = {
+                git_ignored = false,
+                dotfiles = false,
+            },
+        })
+    end,
 }
