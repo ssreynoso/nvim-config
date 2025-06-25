abe0970132c402eb04ff32b756822e783e259d50
@ssreynoso <s.reynosodunjo@gmail.com>
2025-06-25 20:02:44 -0300

    Mejora en comentarios

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,47 +1,50 @@
 -- ~/.config/nvim/ly ua/plugins/treesitter.lua
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		vim.g.skip_ts_context_commentstring_module = true
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "javascript",
+                "typescript",
+                "tsx", -- JSX/TSX (React)
+                "html",
+                "css",
+                "json",
+                "jsonc", -- para archivos como tsconfig.json o .eslintrc
+                "yaml", -- por ejemplo en workflows o configs
+                "lua", -- por tu config de neovim
+                "bash", -- útil para scripts y terminal
+                "markdown", -- para documentación o notas
+                "markdown_inline",
+                "vim", -- ayuda con sintaxis de vimrc o plugins antiguos
+                "query", -- requerido por treesitter para plugins que analizan árboles
+            },
 
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"tsx", -- JSX/TSX (React)
-				"html",
-				"css",
-				"json",
-				"jsonc", -- para archivos como tsconfig.json o .eslintrc
-				"yaml", -- por ejemplo en workflows o configs
-				"lua", -- por tu config de neovim
-				"bash", -- útil para scripts y terminal
-				"markdown", -- para documentación o notas
-				"markdown_inline",
-				"vim", -- ayuda con sintaxis de vimrc o plugins antiguos
-				"query", -- requerido por treesitter para plugins que analizan árboles
-			},
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
+            indent = {
+                enable = true,
+            },
 
-			indent = {
-				enable = true,
-			},
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    node_decremental = "grm",
+                },
+            },
 
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					node_decremental = "grm",
-				},
-			},
-		})
-	end,
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false, -- dejalo en false si usás pre_hook
+            },
+        })
+    end,
 }
--- lua/plugins/treesitter-playground.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-treesitter/playground",
-	cmd = {
-		"TSPlaygroundToggle",
-		"TSHighlightCapturesUnderCursor",
-		"TSNodeUnderCursor",
-	},
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-}
