a1c21977238341b089e31a00a6e5fc0ef6761b71
@ssreynoso <s.reynosodunjo@gmail.com>
2025-07-14 21:04:15 -0300

    Enhance nvim-colorizer to support all file types and color formats
    - Enable colorizer for all file types instead of just CSS/HTML/Vue/TSX/JSX
    - Add support for RGB, RRGGBB, RRGGBBAA hex codes
    - Enable HSL and RGB function highlighting
    - Switch from filetype-specific to event-based loading for better performance

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,13 +1,19 @@
 return {
     "norcalli/nvim-colorizer.lua",
-    ft = { "css", "scss", "html", "vue", "tsx", "jsx" },
+    event = "BufReadPost",
     config = function()
         require("colorizer").setup({
-            css = { css = false, css_fn = false },
-            html = { names = false, rgb_fn = true },
-            scss = { rgb_fn = true },
-            vue = { rgb_fn = true },
-            tsx = { rgb_fn = true },
+            "*", -- Enable for all filetypes
+        }, {
+            RGB = true,      -- #RGB hex codes
+            RRGGBB = true,   -- #RRGGBB hex codes
+            names = false,   -- "Name" codes like Blue
+            RRGGBBAA = true, -- #RRGGBBAA hex codes
+            rgb_fn = true,   -- CSS rgb() and rgba() functions
+            hsl_fn = true,   -- CSS hsl() and hsla() functions
+            css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            mode = "background", -- Set the display mode
         })
 
         vim.keymap.set("n", "<leader>tc", function()
