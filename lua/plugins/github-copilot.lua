ddf91b86705e906ba4b56a88f963c187507dbb78
@ssreynoso <s.reynosodunjo@gmail.com>
2025-08-03 04:31:36 -0300

    Update GitHub Copilot configuration with custom keymaps
    - Change lazy loading from false to InsertEnter event
    - Disable default Tab mapping and remap manually for full acceptance
    - Add Ctrl-l mapping for word-by-word acceptance
    - Add Spanish comments for configuration clarity

--- lua/plugins/github-copilot.lua
+++ lua/plugins/github-copilot.lua
@@ -1,4 +1,16 @@
+-- ~/.config/nvim/lua/plugins/copilot.lua
 return {
     "github/copilot.vim",
-    lazy = false,
+    event = "InsertEnter",
+    config = function()
+        -- 1. Desactivo el <Tab> que pone Copilot
+        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_assume_mapped = true -- evita que Copilot se queje
+
+        -- 2. Vuelvo a poner <Tab> a mano (acepta todo)
+        vim.keymap.set("i", "<Tab>", "copilot#Accept('<CR>')", { expr = true, silent = true, replace_keycodes = false })
+
+        -- 3. Nuevo atajo: aceptar palabra
+        vim.keymap.set("i", "<C-l>", "copilot#AcceptWord()", { expr = true, silent = true })
+    end,
 }
